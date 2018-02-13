f4a1a6c9fb5d3c1410fa77aed8769ffd5a232bb5
Hector <carrfanegmail.com>
2018-02-13 18:38:46 +0000

    adding restaurant to connections

--- app/models/merge_restaurant.rb
+++ app/models/merge_restaurant.rb
@@ -1,10 +1,4 @@
 class MergeRestaurant < ApplicationRecord
-<<<<<<< 776d89ca44522ba54d7536f2443bf7a84354cc60
-  belongs_to :connection
-  has_many :merge_local
-  has_many :prov_merge_rests
-  has_many :providers, through: :prov_merge_rests
-=======
 	#relaciones
 	belongs_to :connection
 	has_many :merge_locals
@@ -13,5 +7,4 @@ class MergeRestaurant < ApplicationRecord
 	#validaciones
 	validates :restaurant_key, presence: true, uniqueness: true
 	validates :connection_id, presence: true, uniqueness: true
->>>>>>> migraciones de connection, merge_local, merge_restaurant, prov_merge_rest, provider con test en rspec
 end
