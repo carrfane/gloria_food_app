38de67a5c7fdbe6354ed161b97ca75dbba29bc15
wilsonsayago <sayagowilson92gmail.com>
2018-02-13 17:49:40 +0000

    test

--- spec/models/connection_spec.rb
+++ spec/models/connection_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Connection, type: :model do
 	context 'validations' do
-		it { should validate_presence_of(:name) }
+		it { should validate_presence_of(:name)  }
 	end
 end
